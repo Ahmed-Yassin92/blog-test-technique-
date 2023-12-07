ed50aa68a8e6982e4e9cfb80d09f5438a5873c04
Ahmed-Yassin92 <93595617+Ahmed-Yassin92@users.noreply.github.com>
2023-12-07 04:50:02 +0100

    updatefiles

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(RegistrationFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Enregistrement de l'utilisateur dans la base de données
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            // Redirection vers la page d'accueil après l'inscription
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'form' => $form->createView(),
         ]);
     }
 }
